0305c4d85f74dfd082542396c1713c94e033aae2
Aleksandar.G <72451741+Aleksandar-G@users.noreply.github.com>
2022-05-11 16:48:00 +0200

    refactor(jwtController.js): add username and userId to payload on verify and lint the code

--- controllers/jwtController.js
+++ controllers/jwtController.js
@@ -1,48 +1,73 @@
-require('dotenv').config()
-const jwt = require("jsonwebtoken")
+require("dotenv").config();
+const jwt = require("jsonwebtoken");
 
 // env variables
-const secret = process.env.SECRET
-const issuer = process.env.ISSUER
+const secret = process.env.SECRET;
+const issuer = process.env.ISSUER;
 
 //verify token
-const verifyJWT = (token, username, client) => {
+const verifyJWT = (token, client) => {
+  // client.db("usernames").collection("usernames").findOne({ username: username }).then((document) => {
+  //     if (document === null) {
+  //         return false
+  //     } else {
+  //         jwt.verify(token, secret, { algorithms: ['HS256'], issuer: issuer }, (err, token) => {
+  //             if (err) {
+  //                 console.log(err.message);
+  //                 return false
+  //             } else {
+  //                 return token
+  //             }
 
-    client.db("usernames").collection("usernames").findOne({ username: username }).then((document) => {
-        if (document === null) {
-            return false
-        } else {
-            jwt.verify(token, secret, { algorithms: ['HS256'], issuer: issuer }, (err, token) => {
-                if (err) {
-                    console.log(err.message);
-                    return false
-                } else {
-                    return token
-                }
-
-            });
-        }
-    })
-
-
-
-
-}
+  //         });
+  //     }
+  // })
+  //console.log(token);
+  return jwt.verify(token, secret, { issuer: issuer }, (err, token) => {
+    if (err) {
+      console.log(err.message);
+      return "";
+    } else {
+      console.log(token);
+      const userData = JSON.stringify({
+        username: token.username,
+        userId: token.userId,
+      });
+      return userData;
+    }
+  });
+};
 
 //generate token
 //generate JWT token using SHA256
-const generateJWT = (username, client) => {
+const generateJWT = (message, client) => {
+  console.log(message);
+  const messageJSON = JSON.parse(message);
+  const token = jwt.sign(
+    { username: messageJSON.username, userId: messageJSON.userId },
+    secret,
+    {
+      expiresIn: "1h",
+      algorithm: "HS256",
+      issuer: issuer,
+    }
+  );
 
-    const token = jwt.sign({ username: username }, secret, { expiresIn: '1h', algorithm: 'HS256', issuer: issuer })
+  client
+    .db("usernames")
+    .collection("usernames")
+    .findOne({ username: messageJSON.username })
+    .then((document) => {
+      if (document === null) {
+        client
+          .db("usernames")
+          .collection("usernames")
+          .insertOne({ username: messageJSON.username });
+      }
+    });
 
-    client.db("usernames").collection("usernames").findOne({ username: username }).then((document) => {
-        if (document === null) {
-            client.db("usernames").collection("usernames").insertOne({ username: username })
-        }
-    })
+  return token;
+};
 
-    return token
-}
-
-exports.verifyJWT = verifyJWT
-exports.generateJWT = generateJWT
+exports.verifyJWT = verifyJWT;
+exports.generateJWT = generateJWT;
